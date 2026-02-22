b5a75e4ae8ac8f0d2c0ee8457f9cd20cc57931b1
jugoouttahere <gangnamstylelive123@gmail.com>
2026-02-22 10:23:49 +0300

    Init

---
+++ src/main/java/ru/rostislav/WeatherViewerApplication.java
@@ -0,0 +1,13 @@
+package ru.rostislav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rostislav/weather_viewer/WeatherViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rostislav.weather_viewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
